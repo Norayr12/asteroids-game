94b51c1184bb82c0b283c5cf87912ea6f1698782
Norayr <norajrhacatran8@gmail.com>
2021-09-21 18:09:25 +0400

    Player movement complete

---
+++ Assets/Scripts/BoundChecker.cs
@@ -0,0 +1,93 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoundChecker : MonoBehaviour
+{
+    [SerializeField] private OutOfBoundsObjectType _objectType; 
+
+    private Vector2 _sceneSize;
+    private SpriteRenderer _spriteRenderer;
+
+    void Awake()
+    {
+        _sceneSize = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, 0));
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    void FixedUpdate()
+    {
+        if (transform.position.x - _spriteRenderer.bounds.size.x / 2 > _sceneSize.x)
+        {
+
+            if (_objectType == OutOfBoundsObjectType.Teleportable)
+                Teleport(BoundType.Right);
+            else
+                Destroy();
+        }
+        else if(transform.position.x + _spriteRenderer.bounds.size.x / 2 < -_sceneSize.x)
+        {
+
+            if (_objectType == OutOfBoundsObjectType.Teleportable)
+                Teleport(BoundType.Left);
+            else
+                Destroy();
+        }
+        else if (transform.position.y - _spriteRenderer.bounds.size.y / 2 > _sceneSize.y)
+        {
+
+            if (_objectType == OutOfBoundsObjectType.Teleportable)
+                Teleport(BoundType.Top);
+            else
+                Destroy();
+        }
+        else if (transform.position.y + _spriteRenderer.bounds.size.y / 2 < -_sceneSize.y)
+        {
+
+            if (_objectType == OutOfBoundsObjectType.Teleportable)
+                Teleport(BoundType.Bottom);
+            else
+                Destroy();
+        }
+
+    }
+
+    private void Teleport(BoundType boundType)
+    {
+        switch (boundType)
+        {
+            case BoundType.Top:
+                transform.position -= new Vector3(0, _sceneSize.y * 2 + _spriteRenderer.bounds.size.y);
+                break;
+
+            case BoundType.Bottom:
+                transform.position += new Vector3(0, _sceneSize.y * 2 + _spriteRenderer.bounds.size.y);
+                break;
+
+            case BoundType.Left:
+                transform.position += new Vector3(_sceneSize.x * 2 + _spriteRenderer.bounds.size.x, 0);
+                break;
+
+            case BoundType.Right:
+                transform.position -= new Vector3(_sceneSize.x * 2 + _spriteRenderer.bounds.size.x, 0);
+                break;
+        }
+    }
+
+    private void Destroy() => Destroy(gameObject);
+}
+
+public enum OutOfBoundsObjectType
+{ 
+    Teleportable,
+    Destroyable
+}
+
+public enum BoundType
+{
+    Top,
+    Bottom,
+    Left,
+    Right
+}
+
--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -4,24 +4,45 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    [Header("Player constants")]
+    [SerializeField] private float _forcePower;
+    [SerializeField] private float _rotationSpeed;
+
+    [Space]
+
+    [Header("Input values")]
     [SerializeField] private KeyCode _forwardAcceleration;
     [SerializeField] private KeyCode _leftRotation;
     [SerializeField] private KeyCode _rightRotation;
     [SerializeField] private KeyCode _shooting;
 
+    private Rigidbody2D _playerRigidbody;
+    private ShootingController _shootingController;
+
+    void Awake()
+    {
+        _playerRigidbody = GetComponent<Rigidbody2D>();
+        _shootingController = GetComponent<ShootingController>();
+    }
+
     void FixedUpdate()
     {
-        
+        if (Input.GetKey(_forwardAcceleration))
+            AddForce();
+
+        if (Input.GetKey(_leftRotation))
+            RotateLeft();
+        else if (Input.GetKey(_rightRotation))
+            RotateRight();
+        else if (Input.GetKeyDown(_shooting))
+            _shootingController.Shoot();
+
     }
 
-    private void RotateLeft()
-    {
+    private void AddForce() => _playerRigidbody.AddForce(transform.up * _forcePower, ForceMode2D.Force);
 
-    }
+    private void RotateLeft() => transform.Rotate(0, 0, 1 * _rotationSpeed);
 
-    private void RotateRight()
-    {
-
-    }
+    private void RotateRight() => transform.Rotate(0, 0, -1 * _rotationSpeed);
 
 }
