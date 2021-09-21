9879f805ece58f94932a8c4677d5b1a68a4c7419
Norayr <norajrhacatran8@gmail.com>
2021-09-21 15:13:30 +0400

    Initial commit

---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private KeyCode _forwardAcceleration;
+    [SerializeField] private KeyCode _leftRotation;
+    [SerializeField] private KeyCode _rightRotation;
+    [SerializeField] private KeyCode _shooting;
+
+    void FixedUpdate()
+    {
+        
+    }
+
+    private void RotateLeft()
+    {
+
+    }
+
+    private void RotateRight()
+    {
+
+    }
+
+}
